4f12f720735a282a9705eded85c3c6526ec622a0
Bob Blinker <BOBtheBlinker@gMail.com>
2024-10-17 08:42:09 -0400

    Initial tweaks & Adjustments

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_avatrn.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_avatrn-user \
+    twrp_avatrn-userdebug \
+    twrp_avatrn-eng
