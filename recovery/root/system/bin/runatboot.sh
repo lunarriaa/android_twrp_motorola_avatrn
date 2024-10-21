9e941d6466e950227c4034593bbe003c44bddec3
Bob Blinker <BOBtheBlinker@gMail.com>
2024-10-21 00:34:22 -0400

    Mr BA PizzaG fixed up some USB

--- recovery/root/system/bin/runatboot.sh
+++ recovery/root/system/bin/runatboot.sh
@@ -1,21 +1,24 @@
 #!/system/bin/sh
 
+# Set SeLinux Permissions
+setenforce 0
+
 mount /vendor_dlkm
 mount /vendor
-modprobe -d /vendor/lib/modules /vendor/lib/modules/nova_0flash_mmi.ko
-modprobe -d /vendor/lib/modules /vendor/lib/modules/ili9882_mmi.ko
+modprobe -d /vendor/lib/modules /vendor/lib/modules/goodix_brl_mmi.ko
+modprobe -d /vendor/lib/modules /vendor/lib/modules/touchscreen_mmi.ko
 sleep 1
-if [ $(cat /sys/class/touchscreen/primary/productinfo) == "NT36672C" ]
+if [ $(cat /sys/class/touchscreen/primary/productinfo) == "gt9916S" ]
 then
 echo 1 > /sys/class/touchscreen/primary/forcereflash
-echo novatek_ts-csot-NT36672C-2302240D-605b-genevn.bin > /sys/class/touchscreen/primary/doreflash
-echo "Reflashing firmware..."
+echo goodix-boe-gt9916S-24010501-6597AA91-avatrn.bin > /sys/class/touchscreen/primary/doreflash
+echo "Reflashing Firmware..."
 echo 0 > /sys/class/touchscreen/primary/forcereflash
 fi
 
 is_fastboot=$(getprop init.svc.fastbootd)
 if [ "$is_fastboot" != "running" ]; then
-echo "Fastboot is running"
+echo "Fastboot Is Running..."
 fi
 
 mkdir /firmware
@@ -24,3 +27,8 @@ mount /dev/block/bootdevice/by-name/modem$SLOT /firmware -O ro
 echo "1" > /proc/sys/kernel/firmware_config/force_sysfs_fallback
 echo "1" > /sys/kernel/boot_adsp/boot
 exit 0
+
+
+
+
+
